017585c86559410c8ec07428014fb1575ccdba78
Renato Lui Geh <renatogeh@gmail.com>
2017-11-24 02:42:07 -0200

    Fixing parser.

--- parser/parser.py
+++ parser/parser.py
@@ -3,13 +3,24 @@
 
 from imdb import IMDb
 import re
+import sys
+import unicodedata
+
+if len(sys.argv) != 3:
+  print("Usage: %s user password\n" % sys.argv[0])
+  print("  user     - db user\n")
+  print("  password - db password\n")
+  exit(0)
+
+user = sys.argv[1]
+password = sys.argv[2]
 
 # IMDb access.
-I = IMDb('sql', uri='postgres://localhost/imdb')
+I = IMDb('sql', uri=('mysql://%s:%s@localhost/imdb' % (user, password)))
 
-# Initial list of people.
-i_list = ['Uma Thurman', 'Harvey Keitel', 'Bill Murray', 'Frances McDormand', 'Quentin Tarantino',\
-    'Wes Anderson']
+# Initial list of people. (I) guarantees IMDbpy gets the most famous amongst them.
+i_list = ['Uma Thurman (I)', 'Harvey Keitel (I)', 'Bill Murray (I)', 'Frances McDormand (I)', \
+    'Quentin Tarantino (I)', 'Wes Anderson (I)']
 
 q_movs = []
 
@@ -23,11 +34,17 @@ movs = {}
 # Agent (i.e. actor or director) movie references. Each entry is [role, movieID].
 agt_refs = {}
 
+def valid_movie(m):
+  if (not m.get('episodes')) and m.get('director') and m.get('year') and m.get('cast') and \
+      (m.get('kind') == 'movie'):
+      return True
+  return False
+
 # Select only movies. Discard TV series.
 def validate_movs(ai, r, m):
   I.update(m)
-  i = m['movieID']
-  if (not 'episodes' in m) and (not i in movs):
+  i = m.movieID
+  if valid_movie(m) and (not i in movs):
     movs[i] = m
     q_movs.append(m)
     if not ai in agt_refs:
@@ -36,7 +53,7 @@ def validate_movs(ai, r, m):
 
 # Adds person e's movies from l to dict d.
 def add_to(l, d, e, r, m=None):
-  i = e['personID']
+  i = e.personID
   if l and (not i in d):
     d[i] = e
     if m is None:
@@ -59,51 +76,74 @@ def retrieve_initial():
     add_to(d, dirs, agt, 'd')
     add_to(a, acts, agt, 'a')
 
+print("Retrieving initial actors and their movies...")
 # Take roots defined by i_list.
 retrieve_initial()
 
+print("Retrieving secondary actors and directors...")
 # Search through movies and add them to their respective dictionaries.
 for m in q_movs:
-  i = m['movieID']
+  i = m.movieID
   # Take all actors and actresses from movie m.
-  l_acts = m['cast']
-  for a in l_acts:
-    add_to(True, acts, a, 'a', i)
+  l_acts = m.get('cast')
+  if l_acts:
+    for a in l_acts:
+      add_to(True, acts, a, 'a', i)
   # Take all directors from movie m.
-  l_dirs = m['director']
-  for d in l_dirs:
-    add_to(True, dirs, d, 'd', i)
+  l_dirs = m.get('director')
+  if l_dirs:
+    for d in l_dirs:
+      add_to(True, dirs, d, 'd', i)
 
 q_movs = None
+
+pattern = re.compile('[\W_]+', re.UNICODE)
+def alphanumify(s):
+  s = s.split()
+  for i in range(0, len(s)):
+    t = s[i]
+    if type(s[i]) == unicode:
+      t = unicodedata.normalize('NFKD', s[i]).encode('ascii', 'ignore')
+    s[i] = pattern.sub('', t)
+  u = ""
+  for i in range(0, len(s)-1):
+    u += s[i] + " "
+  u += s[len(s)-1]
+  return u
 
 # Applies a camelCase format to string s.
 def camelify(s):
   # Get rid of special characters.
-  p = re.compile('[\W_]+')
-  s = p.sub('', s)
   # Camelifies.
-  tks = s.split()
+  tks = alphanumify(s).split()
   r = tks[0].lower()
   for i in range(1, len(tks)):
     r += tks[i].capitalize()
   return r
 
-insts_movs = {}
-insts_agts = {}
+# Turns a complex name into a simple firstName, familyName. Returns first, family, first+family.
+def namefy(s):
+  tks = alphanumify(s).split()
+  first, family = tks[0].capitalize(), tks[len(tks)-1].capitalize()
+  return first, family, "%s %s" % (first, family)
+
+inst_movs = {}
+inst_agts = {}
 
 # Initialize inst_* with camelified names/titles.
 def get_inst_names():
   for k, e in movs.iteritems():
-    inst_movs[camelify(str(e['title']))] = k
+    inst_movs[camelify(e['title'])] = k
   for i in agt_refs.iterkeys():
     a = None
     if i in acts:
       a = acts[i]
     else:
       a = dirs[i]
-    _, _, ff = namefy(str(a['name']))
+    _, _, ff = namefy(a['name'])
     inst_agts[camelify(ff)] = i
 
+print("Initializing instance names...")
 get_inst_names()
 
 # Prints different instances.
@@ -111,20 +151,24 @@ def print_diff_inst(t, d, f):
   T = []
   for k, _ in d.iteritems():
     if k != t:
-      f.write("        projeto:%s\n" % v)
+      f.write("        projeto:%s\n" % k)
 
 # Prints every movie's OWL representation.
-def print_movies(f):
+def print_movies(f, cm, st):
   for _, m in movs.iteritems():
-    t = str(m['title'])
+    t = alphanumify(m['title'])
     y = m['year']
     ct = camelify(t)
     f.write("Individual: projeto:%s\n\n" % ct)
     f.write("    Types:\n        projeto:Movie\n\n")
     f.write("    Facts:\n     projeto:movieTitle  \"%s\",\n" % t)
     f.write("     projeto:releaseYear  %d\n\n" % y)
-    f.write("    DifferentFrom:\n")
-    print_diff_inst(ct, inst_movs, f)
+    # f.write("    DifferentFrom:\n")
+    # print_diff_inst(ct, inst_movs, f)
+    # f.write("\n")
+    cm -= 1
+    if cm % st == 0:
+      print("  Scanning movies... [%d/%d]" % (len(movs)-cm, len(movs)))
 
 agents = {}
 
@@ -137,18 +181,62 @@ def merge_ids():
     if not k in agents:
       agents[k] = v
 
+print("Merging IDs from agents (actors and directors)...")
 merge_ids()
 
-# Turns a complex name into a simple firstName, familyName. Returns first, family, first+family.
-def namefy(s):
-  tks = s.split()
-  first, family = tks[0].capitalize(), tks[len(tks)-1].capitalize()
-  return first, family, "%s %s" % (first, family)
+# ---- SQL ----
+
+import MySQLdb
+
+print("Connecting to DB...")
+db = MySQLdb.connect(host="localhost", user=user, passwd=password, db="imdb")
+
+def unfold_gender(c):
+  if c == 'm':
+    return "male"
+  elif c == "f":
+    return "female"
+  else:
+    return "nil"
+
+# Gets the gender of agent of name "First Family".
+def get_gender(first, family):
+  # We assume the following:
+  # If there exists a person p that has name "First Family", and let q be someone with the name
+  # "First Middle Family", then p.gender == q.gender.
+  # We first test for p. If there exists no p, then we obviously omitted Middle. If that is the
+  # case, we search for the Middle name variant.
+  db.query("SELECT gender FROM name N WHERE N.name LIKE '%s, %s';" % (family, first))
+  r = db.store_result()
+  t = r.fetch_row()
+  # Another supposition (corollary to the above). If p and q are two people with the same name,
+  # then we p.gender == q.gender.
+  if len(t) != 0:
+    return unfold_gender(t[0][0])
+
+  # Case 1: "First Family1 Family2"
+  db.query("SELECT gender FROM name N WHERE N.name LIKE '%% %s, %s';" % (family, first))
+  r = db.store_result()
+  t = r.fetch_row()
+  if len(t) != 0:
+    return unfold_gender(t[0][0])
+
+  # Case 2: "First Middle Family"
+  db.query("SELECT gender FROM name N WHERE N.name LIKE '%s, %s %%';" % (family, first))
+  r = db.store_result()
+  t = r.fetch_row()
+  # There must exist a person with a name containing {first, family}.
+  if len(t) != 0:
+    return unfold_gender(t[0][0])
+  else:
+    return "nil"
+
+# ---- SQL ----
 
 # Prints every agent's OWL representation (i.e. actors or directors).
-def print_agents(f):
+def print_agents(f, ca, st):
   for k, a in agents.iteritems():
-    first, family, ff = namefy(str(a['name']))
+    first, family, ff = namefy(a['name'])
     cff = camelify(ff)
     is_d, is_a = k in dirs, k in acts
     f.write("Individual: projeto:%s\n\n" % cff)
@@ -160,17 +248,30 @@ def print_agents(f):
     f.write("\n    Facts:\n")
     roles = agt_refs[k]
     for r in roles:
-      mov = camelify(str(movs[r[1]]['title']))
+      mov = camelify(movs[r[1]]['title'])
       if r[0] == 'd':
         f.write("     projeto:directs  projeto:%s,\n" % mov)
       else:
         f.write("     projeto:actsIn  projeto:%s,\n" % mov)
     f.write("     foaf-modified:familyName  \"%s\",\n" % family)
-    f.write("     foaf-modified:firstName  \"%s\"\n\n" % first)
-    f.write("    DifferentFrom:\n")
-    print_diff_inst(cff, inst_movs, f)
+    f.write("     foaf-modified:firstName  \"%s\"\n" % first)
+    f.write("     foaf-modified:gender  \"%s\"\n\n" % get_gender(first, family))
+    # f.write("    DifferentFrom:\n")
+    # print_diff_inst(cff, inst_agts, f)
+    # f.write("\n\n")
+    ca -= 1
+    if ca % st == 0:
+      print("  Scanning agents... [%d/%d]" % (len(agents)-ca, len(agents)))
 
-with open('insts.owl', 'r') as owl_file:
-  print_movies(owl_file)
-  print_agents(owl_file)
-owl_file.closed()
+step_cmpl = 100
+movs_cmpl = len(movs)
+agts_cmpl = len(agents)
+
+print("Creating OWL file...")
+with open('insts.owl', 'w') as owl_file:
+  print("Exporting movies to OWL...")
+  print_movies(owl_file, movs_cmpl, step_cmpl)
+  print("Exporting agents to OWL...")
+  print_agents(owl_file, agts_cmpl, step_cmpl)
+  print("Closing OWL file...")
+print("Bye.")
--- parser/run_parser.sh
+++ parser/run_parser.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
 
-INSTALL=1
+if [ "$1" == "-h" || "$1" == "--help" || "$#" -lt 2]; then
+  echo "Usage: $0 user password [db] [install]"
+  echo "  user     - DB user"
+  echo "  password - DB password"
+  echo "  db       - 'mysql' or 'postgres' (optional)"
+  echo "  install  - 1 if install and download before, 0 otherwise (optional)"
+  exit 0
+fi
 
-# Assuming Arch Linux. Note that everything in /tmp will be deleted after system shutsdown!
-if [ "$INSTALL" -eq "1" ]; then
-  sudo pacman -S imdbpy python2-psycopg2
+# PostgreSQL takes too long. MySQL is faster.
+DB='mysql'
+
+INSTALL=0
+
+if [ "$#" -eq 3]; then
+  DB="$3"
+fi
+
+if [ "$#" -eq 4]; then
+  INSTALL="$4"
+fi
+
+# Assuming Arch Linux. Note that everything in /tmp will be deleted after system shutdown!
+if [ "$INSTALL" -eq 1 ]; then
+  if [ "$DB" == "mysql" ]; then
+    sudo pacman -S mariadb imdbpy mysql-python
+    echo "Follow https://wiki.archlinux.org/index.php/Mysql and configure DB.."
+  else
+    sudo pacman -S imdbpy python2-psycopg2 postgresql
+    echo "Follow https://wiki.archlinux.org/index.php/PostgreSQL and configure DB."
+  fi
+  read -n1 -r -p "Press any key to continue after configuring..."
+
+  echo "Downloading imdb.tar..."
   link='https://drive.google.com/uc?export=download&confirm=hqJf&id=15io6Ja9zFHmc0kFJ30oqyo_oTiy_xJRT'
   curl "$link" -o /tmp/imdb.tar
   pushd .
+  echo "Extracting .tar..."
   cd /tmp
   tar -xvf imdb.tar
   cd imdb
 
+  echo "Deleting irrelevant .list.gz..."
   exc=('actors' 'actresses' 'directors' 'movies')
   for f in *; do
     n=0
@@ -20,7 +51,7 @@ if [ "$INSTALL" -eq "1" ]; then
         let n=1
       fi
     done
-    if [ "$n" -eq "0" ]; then
+    if [ "$n" -eq 0 ]; then
       rm "$f"
     fi
   done
@@ -28,10 +59,23 @@ if [ "$INSTALL" -eq "1" ]; then
   popd
 fi
 
-# Create DB with PostgreSQL.
-createdb -W imdb
-# Run imdbpy's imdbpy2sql.py, assuming it's path is /usr/bin/ and the directory containing all
-# *.list.gz are located at /tmp/imdb
-python2 /usr/bin/imdbpy2sql.py -d /tmp/imdb/ -u "postgresql://localhost/imdb"
+r=`mysqlshow --user="$1" --password="$2" imdb | grep -v Wildcard | grep -o imdb`
+
+if [ "$DB" == "mysql" ]; then
+  if [ "$r" != "imdb"]; then
+    # Create DB with PostgreSQL.
+    createdb -W imdb
+  fi
+  # Run imdbpy's imdbpy2sql.py, assuming its path is /usr/bin/ and the directory containing all
+  # *.list.gz is located at /tmp/imdb.
+  python2 /usr/bin/imdbpy2sql.py -d /tmp/imdb/ -u "postgresql://localhost/imdb"
+else
+  if [ "$r" != "imdb"]; then
+    # Create DB with MySQL.
+    mysqladmin -p create imdb
+  fi
+  # Run imdbpy2sql.
+  python2 /usr/bin/imdbpy2sql.py -d /tmp/imdb/ -u "mysql://$1:$2@localhost/imdb"
+fi
 # Run parser.
 python2 parser.py
